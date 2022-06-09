58423a702b134832e91df6283b8656b156088b2c
Dillon Winchell <dwinchel@redhat.com>
2022-06-09 11:16:43 -0400

    Readme updates (#22)
    * Updated README instructions for clarity/completeness and to replace some manual changes with copy-pasted commands.
    
    * Go back to using source code for helm charts
    
    Published ploigos-charts are out of date
    
    * Update README.md
    
    * Use terminal commands to expand PVC
    
    * Automated forking gitea repos
    
    * Automated generation of new ConfigMap and Secret
    
    * Remove backup instructions. We won't need them
    
    * Separate GitLab instructions
    
    * Automate adding gitea webhook
    
    * Fix shebang and shell options
    
    * Make sed command portable to mac
    
    * Added comment to generate-config.sh

---
+++ fork-repo-to-gitea.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -ex -o pipefail
+
+GITEA_USER=ploigos
+GITEA_PASS=$(oc get secret ploigos-service-account-credentials -n devsecops -o yaml | yq .data.password | base64 -d)
+REPO_NAME=${1}
+SOURCE_URL=${2}
+
+GITEA_NAMESPACE=devsecops
+GITEA_URL="https://$(oc get route gitea -n ${GITEA_NAMESPACE} -o yaml | yq '.status.ingress[].host')"
+
+GITEA_API_URL=${GITEA_URL}/api/v1
+
+# Get an authentication token for the Gitea REST API
+TOKEN_NAME=$(date +%s%N) # nanoseconds since epoch, to make it unique between executions
+TOKEN_RESPONSE=$(curl -k -XPOST -H "Content-Type: application/json"  -k -d "{\"name\":\"${TOKEN_NAME}\"}" -u ${GITEA_USER}:${GITEA_PASS} ${GITEA_API_URL}/users/${GITEA_USER}/tokens)
+TOKEN=$(echo ${TOKEN_RESPONSE} | yq e .sha1)
+
+# Get the id of the Gitea organization that the new repo will belong to
+TARGET_ORG=platform
+ORG_ID=$( \
+  curl -k 'GET' \
+  "${GITEA_API_URL}/orgs/${TARGET_ORG}" \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -H "Authorization: token ${TOKEN}" \
+  | yq e .id
+)
+
+# Fork the upstream repo into Gitea
+curl -k -X 'POST' \
+  "${GITEA_API_URL}/repos/migrate" \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -H "Authorization: token ${TOKEN}" \
+  -d "{
+            \"clone_addr\": \"${SOURCE_URL}\", \
+            \"uid\": ${ORG_ID}, \
+            \"repo_name\": \"${REPO_NAME}\" \
+     }"
+
---
+++ onboard-app.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -ex -o pipefail
+
+GITEA_APP_REPO=${1}
+
+GITEA_ORG=platform
+GITEA_NAMESPACE=devsecops
+
+GITEA_USER=$(oc get secret gitea-admin-credentials -o yaml | yq .data.username | base64 -d)
+GITEA_PASS=$(oc get secret gitea-admin-credentials -o yaml | yq .data.password | base64 -d)
+GITEA_URL="https://$(oc get route gitea -n ${GITEA_NAMESPACE} -o yaml | yq '.status.ingress[].host')"
+GITEA_API_URL=${GITEA_URL}/api/v1
+EVENT_LISTENER_URL="http://$(oc get route el-everything-pipeline -o yaml | yq .status.ingress[].host)/"
+
+
+# Get an authentication token for the Gitea REST API
+TOKEN_NAME=$(date +%s%N) # nanoseconds since epoch, to make it unique between executions
+TOKEN_RESPONSE=$(curl -k -XPOST -H "Content-Type: application/json"  -k -d "{\"name\":\"${TOKEN_NAME}\"}" -u ${GITEA_USER}:${GITEA_PASS} ${GITEA_API_URL}/users/${GITEA_USER}/tokens)
+TOKEN=$(echo ${TOKEN_RESPONSE} | yq e .sha1)
+
+# Create a Gitea webhook to trigger the Tekton pipeline when the demo application code changes
+curl -k \
+"${GITEA_API_URL}/repos/${GITEA_ORG}/${GITEA_APP_REPO}/hooks" \
+-H 'accept: application/json' \
+-H 'Content-Type: application/json' \
+-H "Authorization: token ${TOKEN}" \
+-d "{ \
+  \"type\": \"gitea\", \
+  \"active\": true, \
+  \"config\": {\"content_type\": \"json\", \"url\": \"${EVENT_LISTENER_URL}\"} \
+}"
+
